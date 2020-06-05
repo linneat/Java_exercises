de31b8a7a1aff248e68d841b22f46074e445b9eb
linneat <51674082+linneat@users.noreply.github.com>
2020-06-05 09:49:33 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-public class Student extends Person {
+public class Student extends Person implements Comparable<Student> {
   
   String uniName;
   String studentNr;
@@ -16,5 +16,21 @@ public class Student extends Person {
     String fullName = firstName + " " + lastName.toUpperCase();
     return fullName;
   }
+
+  public int compareTo(Student other)
+  {
+    //  It is used to compare the current object with the specified object. It returns
+
+    //  positive integer, if the current object is greater than the specified object.
+    //  negative integer, if the current object is less than the specified object.
+    //  zero, if the current object is equal to the specified object.
+    
+    if (this.age == other.age)  
+      return 0;  
+    else if (this.age > other.age)  
+      return 1;  
+    else  
+      return -1;  
+  }    
 
 }
